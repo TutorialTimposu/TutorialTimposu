594e1e8c0d2b8f67b32c70ca137b63e1452eb31f
Ucup Topekox <ucup.topekox@gmail.com>
2023-06-02 18:08:04 +0800

    update gtag

--- static/gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-P5TXTDT91N"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-P5TXTDT91N');
-</script>
